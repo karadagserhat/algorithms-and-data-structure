ab1b059ac1c9174eea013e6946a02d5515281248
karadagserhat <serhatkaradagg@outlook.com>
2023-11-07 18:41:16 +0300

    fixed problems

--- a.js
+++ a.js
@@ -68,25 +68,13 @@
 /********* */
 // 28- merge sort
 
-function mergeSort(arr) {
-  if (arr.length === 1) return arr;
-
-  const center = Math.floor(arr.length / 2);
-  const left = arr.slice(0, center);
-  const right = arr.slice(center);
-
-  return merge(mergeSort(left), mergeSort(right));
-}
-
-function merge(left, right) {
-  const results = [];
-
-  while (left.length && right.length) {
-    if (left[0] < right[0]) results.push(left.shift());
-    else results.push(right.shift());
+function bb(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    }
   }
-
-  return [...results, ...left, ...right];
+  return arr;
 }
 
-console.log(mergeSort([1, 2, 3, 55, 3, 44, 66, -12]));
+console.log(ss([5, 3, 1, 7, 54, 34]));
