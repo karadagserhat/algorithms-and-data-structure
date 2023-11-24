d4395fafec209d9af29630eb64a3e33b9cfbbd96
karadagserhat <serhatkaradagg@outlook.com>
2023-11-24 16:05:06 +0300

    fixed problems

--- a.js
+++ a.js
@@ -67,14 +67,3 @@
 
 /********* */
 // 28- merge sort
-
-function bb(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-    }
-  }
-  return arr;
-}
-
-console.log(ss([5, 3, 1, 7, 54, 34]));
